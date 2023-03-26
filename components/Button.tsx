f57fa39096952e286c3301b1eb7859a9b944ecc0
joao-arthur <joao.lothamer@gmail.com>
2023-03-26 13:08:58 -0300

    feat: enhance layout

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,13 +1,23 @@
 import { VNode } from "preact";
+import { Next } from "./Icons/Next.tsx";
+import { Pause } from "./Icons/Pause.tsx";
+import { Play } from "./Icons/Play.tsx";
 
 type props = {
     readonly label: string;
     readonly onClick: () => void;
+    readonly icon: "next" | "pause" | "play";
 };
 
-export function Button({ label, onClick }: props): VNode {
+export function Button({ label, onClick, icon }: props): VNode {
     return (
-        <button className="bg-gray-500 my-1" onClick={onClick}>
+        <button
+            className="bg-indigo-500 text-white shadow my-2 rounded flex justify-center gap-1"
+            onClick={onClick}
+        >
+            {icon === "next" ? <Next /> : null}
+            {icon === "pause" ? <Pause /> : null}
+            {icon === "play" ? <Play /> : null}
             {label}
         </button>
     );
--- islands/Canvas.tsx
+++ islands/Canvas.tsx
@@ -62,51 +62,75 @@ export default function Canvas(): VNode {
             />
             <div className="flex flex-col">
                 <div className="flex flex-col">
-                    <label for="gap">Gap: {model?.gap}</label>
-                    <RangeInput
-                        id="gap"
-                        min={0}
-                        max={7}
-                        step={1}
-                        value={model ? model.gap : 0}
-                        setValue={(gap) => controller?.setGap(gap)}
-                    />
+                    <label for="gap">Gap</label>
+                    <div className="flex">
+                        <RangeInput
+                            id="gap"
+                            min={0}
+                            max={5}
+                            step={1}
+                            value={model ? model.gap : 0}
+                            setValue={(gap) =>
+                                controller?.setGap(gap)}
+                        />
+                        <label className="w-6 text-center block">
+                            {model?.gap}
+                        </label>
+                    </div>
                 </div>
                 <div className="flex flex-col">
                     <label for="size">
-                        Size: {model?.model.size}
+                        Size
                     </label>
-                    <RangeInput
-                        id="size"
-                        min={2}
-                        max={100}
-                        step={2}
-                        value={model ? model.model.size : 0}
-                        setValue={(size) => controller?.setSize(size)}
-                    />
+                    <div className="flex">
+                        <RangeInput
+                            id="size"
+                            min={2}
+                            max={100}
+                            step={2}
+                            value={model ? model.model.size : 0}
+                            setValue={(size) =>
+                                controller?.setSize(size)}
+                        />
+                        <label className="w-6 text-center block">
+                            {model?.model.size}
+                        </label>
+                    </div>
                 </div>
                 <div className="flex flex-col">
-                    <label for="fps">FPS: {model?.fps}</label>
-                    <RangeInput
-                        id="fps"
-                        min={1}
-                        max={20}
-                        step={1}
-                        value={model ? model.fps : 0}
-                        setValue={(fps) => controller?.setFps(fps)}
-                    />
+                    <label for="fps">FPS</label>
+                    <div className="flex">
+                        <RangeInput
+                            id="fps"
+                            min={1}
+                            max={99999}
+                            step={1}
+                            value={model ? model.fps : 0}
+                            setValue={(fps) =>
+                                controller?.setFps(fps)}
+                        />
+                        <label className="w-6 text-center block">
+                            {model?.fps}
+                        </label>
+                    </div>
                 </div>
-                <span>
-                    <label>Iteration: {model?.model.iteration}</label>
+                <span className="my-1">
+                    <label>
+                        Iteration: {model?.model.iteration}
+                    </label>
                 </span>
                 <Button
-                    label={model?.status === "resumed"
+                    icon={model?.status === "resumed"
                         ? "pause"
-                        : "resume"}
+                        : "play"}
+                    label={model?.status === "resumed"
+                        ? "PAUSE"
+                        : "RESUME"}
                     onClick={handleToggle}
                 />
                 <Button
-                    label="iterate"
+                    icon="next"
+                    label="ITERATE"
                     onClick={() => controller?.singleIteration()}
                 />
             </div>
