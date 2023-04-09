361a402d66f80800cc403cd6c0334d5e8e4ce924
joao-arthur <joao.lothamer@gmail.com>
2023-04-09 11:19:23 -0300

    fix: zoom refactor

--- game-of-life/src/model/zoom/zoom.ts
+++ game-of-life/src/model/zoom/zoom.ts
@@ -1,21 +1,13 @@
 import { modelType } from "../model.js";
 
 export function zoom(model: modelType, newSize: number): modelType {
-    const halfX = (model.position.x1 + model.position.x2) / 2;
-    const halfY = (model.position.y1 + model.position.y2) / 2;
+    const halfX = Math.floor((model.position.x1 + model.position.x2) / 2);
+    const halfY = Math.floor((model.position.y1 + model.position.y2) / 2);
 
-    const x1Raw = halfX - (newSize / 2);
-    const y1Raw = halfY - (newSize / 2);
-    const x2Raw = halfX + (newSize / 2);
-    const y2Raw = halfY + (newSize / 2);
-
-    const differentQuadrantX = Math.sign(x1Raw) !== Math.sign(x2Raw);
-    const differentQuadrantY = Math.sign(y1Raw) !== Math.sign(y2Raw);
-
-    const x1 = Math.ceil(x1Raw);
-    const y1 = Math.ceil(y1Raw);
-    const x2 = Math.floor(x2Raw);
-    const y2 = Math.floor(y2Raw);
+    const x1 = Math.ceil(halfX - newSize / 2 + 1);
+    const x2 = Math.ceil(halfX + newSize / 2);
+    const y1 = Math.ceil(halfY - newSize / 2 + 1);
+    const y2 = Math.ceil(halfY + newSize / 2);
 
     return {
         value: model.value,
