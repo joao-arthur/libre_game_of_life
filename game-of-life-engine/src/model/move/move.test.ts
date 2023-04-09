4ce0e936cb394ecd2a92a8f0d42b515623edb883
joao-arthur <joao.lothamer@gmail.com>
2023-04-09 20:13:21 -0300

    refactor: move arguments

--- game-of-life-engine/src/model/move/move.test.ts
+++ game-of-life-engine/src/model/move/move.test.ts
@@ -10,28 +10,28 @@ it("move", () => {
     const model = build({ x1: -10, y1: -10, x2: 10, y2: 10 });
 
     expect(
-        move(model, { deltaX: 1, deltaY: 0 }),
+        move(model, { x: 1, y: 0 }),
     ).toEqual(build({ x1: -9, y1: -10, x2: 11, y2: 10 }));
     expect(
-        move(model, { deltaX: -1, deltaY: 0 }),
+        move(model, { x: -1, y: 0 }),
     ).toEqual(build({ x1: -11, y1: -10, x2: 9, y2: 10 }));
     expect(
-        move(model, { deltaX: 0, deltaY: 1 }),
+        move(model, { x: 0, y: 1 }),
     ).toEqual(build({ x1: -10, y1: -9, x2: 10, y2: 11 }));
     expect(
-        move(model, { deltaX: 0, deltaY: -1 }),
+        move(model, { x: 0, y: -1 }),
     ).toEqual(build({ x1: -10, y1: -11, x2: 10, y2: 9 }));
 
     expect(
-        move(model, { deltaX: 11, deltaY: 0 }),
+        move(model, { x: 11, y: 0 }),
     ).toEqual(build({ x1: 1, y1: -10, x2: 21, y2: 10 }));
     expect(
-        move(model, { deltaX: -11, deltaY: 0 }),
+        move(model, { x: -11, y: 0 }),
     ).toEqual(build({ x1: -21, y1: -10, x2: -1, y2: 10 }));
     expect(
-        move(model, { deltaX: 0, deltaY: 11 }),
+        move(model, { x: 0, y: 11 }),
     ).toEqual(build({ x1: -10, y1: 1, x2: 10, y2: 21 }));
     expect(
-        move(model, { deltaX: 0, deltaY: -11 }),
+        move(model, { x: 0, y: -11 }),
     ).toEqual(build({ x1: -10, y1: -21, x2: 10, y2: -1 }));
 });
--- game-of-life-engine/src/model/move/move.ts
+++ game-of-life-engine/src/model/move/move.ts
@@ -1,22 +1,18 @@
+import { cartesianPointType } from "../../cartesianPlane/mod.js";
 import { modelType } from "../model.js";
-
-type changeType = {
-    readonly deltaX: number;
-    readonly deltaY: number;
-};
 
 export function move(
     model: modelType,
-    change: changeType,
+    change: cartesianPointType,
 ): modelType {
     return {
         value: model.value,
         iteration: model.iteration,
         position: {
-            x1: model.position.x1 + change.deltaX,
-            y1: model.position.y1 + change.deltaY,
-            x2: model.position.x2 + change.deltaX,
-            y2: model.position.y2 + change.deltaY,
+            x1: model.position.x1 + change.x,
+            y1: model.position.y1 + change.y,
+            x2: model.position.x2 + change.x,
+            y2: model.position.y2 + change.y,
         },
     };
 }
