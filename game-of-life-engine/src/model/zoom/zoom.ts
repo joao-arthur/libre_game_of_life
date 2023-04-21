9b0a47843acde15dca4bb8ff9e03d0217b871d25
joao-arthur <joao.lothamer@gmail.com>
2023-04-21 10:09:20 -0300

    feat: new zoom api

--- game-of-life-engine/src/model/zoom/zoom.ts
+++ game-of-life-engine/src/model/zoom/zoom.ts
@@ -1,21 +1,23 @@
 import { modelType } from "../model.js";
 
 export function zoom(model: modelType, newSize: number): modelType {
-    const halfX = Math.floor(
-        (model.position.x1 + model.position.x2) / 2,
-    );
-    const halfY = Math.floor(
-        (model.position.y1 + model.position.y2) / 2,
-    );
+    const halfNewSize = newSize / 2;
+    const halfX = (model.position.x1 + model.position.x2) / 2;
+    const halfY = (model.position.y1 + model.position.y2) / 2;
 
-    const x1 = Math.ceil(halfX - newSize / 2 + 1);
-    const x2 = Math.ceil(halfX + newSize / 2);
-    const y1 = Math.ceil(halfY - newSize / 2 + 1);
-    const y2 = Math.ceil(halfY + newSize / 2);
+    const x1 = Math.ceil(halfX - halfNewSize);
+    const y1 = Math.ceil(halfY - halfNewSize);
+    const x2 = x1 + newSize - 1;
+    const y2 = y1 + newSize - 1;
 
     return {
         value: model.value,
         iteration: model.iteration,
-        position: { x1, y1, x2, y2 },
+        position: {
+            x1: x1 === -0 ? 0 : x1,
+            y1: y1 === -0 ? 0 : y1,
+            x2: x2 === -0 ? 0 : x2,
+            y2: y2 === -0 ? 0 : y2,
+        },
     };
 }
