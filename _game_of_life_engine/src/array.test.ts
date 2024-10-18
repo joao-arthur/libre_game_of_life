d1bf65a81e458734b52d99b90fb91bedb23a6026
joao-arthur <joao.lothamer@gmail.com>
2024-10-17 22:09:26 -0300

    wip: delete old implementation

--- _game_of_life_engine/src/array.test.ts
+++
@@ -1,6 +0,0 @@
-import { assert, test } from "vitest";
-import { arrPosFrom } from "./array.js";
-
-test("arrPosFrom", () => {
-    assert.deepStrictEqual(arrPosFrom(20, -13), { row: 20, col: -13 });
-});
--- _game_of_life_engine/src/array.ts
+++
@@ -1,8 +0,0 @@
-export type ArrPos = {
-    readonly row: number;
-    readonly col: number;
-};
-
-export function arrPosFrom(row: number, col: number): ArrPos {
-    return { row, col };
-}
