93287d125b8fc4e8e817c77519241e8e47c4744c
joao-arthur <joao.lothamer@gmail.com>
2025-06-07 20:24:32 -0300

    wip(frontend): wasm integration

--- web_frontend/src/routes/+page.svelte
+++ web_frontend/src/routes/+page.svelte
@@ -1,11 +1,44 @@
 <script lang="ts">
+    import { onMount } from "svelte";
+    import initWASM, { engineGetPresets, EngineStatus, } from "game_of_life_engine";
     import Button from "$lib/components/Button.svelte";
     import RangeInput from "$lib/components/RangeInput.svelte";
     import Select from "$lib/components/Select.svelte";
 
+    let initiated = $state(false);
+    let presets = $state([]);
 
+    onMount(() => {
+        if (!initiated) {
+            initiated = true;
+            initWASM().then(() => {
+                presets = engineGetPresets();
+            });
+        }
+    });
 
+    function onClick(): void {}
 
+    function handleZoomTo(size: number) {}
+
+    function handleSetGap(gap: number) {}
+
+    function handleSetFPS(fps: number) {}
+
+    function handleSetPreset(preset: string) {}
+
+    function handleToggle(): void {}
+
+    function handleIterate() {}
+
+    const model = {
+        size: 500,
+        fps: 60,
+        gap: 0,
+        preset: "block",
+        age: 0n,
+        status: EngineStatus.Paused,
+    };
 </script>
 
 <style>
@@ -37,79 +70,85 @@
     .input-value {
         width: 4rem;
         text-align: center;
-        display: block; 
+        display: block;
     }
-    </style>
+</style>
 
-        <main>
-            <canvas onclick={onClick} width={dimension} height={dimension} style={{ width: dimension, height: dimension }}></canvas>
-            <div>
-                <div class="form">
-                    <label for="preset">Preset</label>
-                    <Select
-                        id="preset"
-                        groups={presets.map((group: any) => ({
-                            label: group.info.name,
-                            value: group.info.id,
-                            options: group.items.map((item: any) => ({
-                                label: item.name,
-                                value: item.id,
-                            })),
-                        }))}
-                        value={model?.preset || ""}
-                        onChange={handleSetPreset}
-                    />
-                </div>
-                <div class="field-container">
-                    <label for="gap">Gap</label>
-                    <div class="input-container">
-                        <RangeInput
-                            id="gap"
-                            min={0}
-                            max={2}
-                            step={1}
-                            value={model ? model.gap : 0}
-                            onChange={handleSetGap}
-                        />
-                        <label class="input-value">{model ? model.gap : 0}</label>
-                    </div>
-                </div>
-                <div class="field-container">
-                    <label for="size">Size</label>
-                    <div class="input-container">
-                        <RangeInput
-                            id="size"
-                            min={2 + (model ? model.size % 2 === 0 ? 0 : 1 : 0)}
-                            max={200 + (model ? model.size % 2 === 0 ? 0 : 1 : 0)}
-                            step={2}
-                            value={model ? model.size : 0}
-                            onChange={handleZoomTo}
-                        />
-                        <label class="input-value">{model ? model.size : 0}</label>
-                    </div>
-                </div>
-                <div class="field-container">
-                    <label for="fps">FPS</label>
-                    <div class="input-container">
-                        <RangeInput
-                            id="fps"
-                            min={1}
-                            max={60}
-                            step={1}
-                            value={model ? model.fps : 0}
-                            onChange={handleSetFPS}
-                        />
-                        <label class="input-value">{model ? model.fps : 0}</label>
-                    </div>
-                </div>
-                <span className="my-1">
-                    <label>Iteration: {model ? Number(model.age) : 0}</label>
-                </span>
-                <Button
-                    icon={model?.status === EngineStatus.Resumed ? "pause" : "play"}
-                    label={model?.status === EngineStatus.Resumed ? "PAUSE" : "RESUME"}
-                    onClick={handleToggle}
+<main>
+    <canvas
+        onclick={onClick}
+        width={300}
+        height={300}
+        style={`width: ${300}px; height: ${300}px;`}
+    >
+    </canvas>
+    <div>
+        <div class="form">
+            <label for="preset">Preset</label>
+            <Select
+                id="preset"
+                groups={presets.map((group: any) => ({
+                    label: group.info.name,
+                    value: group.info.id,
+                    options: group.items.map((item: any) => ({
+                        label: item.name,
+                        value: item.id,
+                    })),
+                }))}
+                value={model?.preset || ""}
+                onChange={handleSetPreset}
+            />
+        </div>
+        <div class="field-container">
+            <label for="gap">Gap</label>
+            <div class="input-container">
+                <RangeInput
+                    id="gap"
+                    min={0}
+                    max={2}
+                    step={1}
+                    value={model ? model.gap : 0}
+                    onChange={handleSetGap}
                 />
-                <Button icon="next" label="ITERATE" onClick={handleIterate} />
+                <label class="input-value">{model ? model.gap : 0}</label>
             </div>
-        </main>
+        </div>
+        <div class="field-container">
+            <label for="size">Size</label>
+            <div class="input-container">
+                <RangeInput
+                    id="size"
+                    min={2 + (model ? model.size % 2 === 0 ? 0 : 1 : 0)}
+                    max={200 + (model ? model.size % 2 === 0 ? 0 : 1 : 0)}
+                    step={2}
+                    value={model ? model.size : 0}
+                    onChange={handleZoomTo}
+                />
+                <label class="input-value">{model ? model.size : 0}</label>
+            </div>
+        </div>
+        <div class="field-container">
+            <label for="fps">FPS</label>
+            <div class="input-container">
+                <RangeInput
+                    id="fps"
+                    min={1}
+                    max={60}
+                    step={1}
+                    value={model ? model.fps : 0}
+                    onChange={handleSetFPS}
+                />
+                <label class="input-value">{model ? model.fps : 0}</label>
+            </div>
+        </div>
+        <span>
+            <label>Iteration: {model ? Number(model.age) : 0}</label>
+        </span>
+        <Button
+            icon={model?.status === 0 ? "pause" : "play"}
+            label={model?.status === 0 ? "PAUSE" : "RESUME"}
+            onClick={handleToggle}
+        />
+        <Button icon="next" label="ITERATE" onClick={handleIterate} />
+    </div>
+</main>
--- web_frontend/vite.config.ts
+++ web_frontend/vite.config.ts
@@ -1,6 +1,10 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import wasm from "vite-plugin-wasm";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+    plugins: [
+        sveltekit(),
+        wasm(),
+    ]
 });
