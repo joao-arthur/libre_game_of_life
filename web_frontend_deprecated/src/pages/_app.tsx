a7139b3e2f74e14d52f020e4a28a91cc9f8afd9e
joao-arthur <joao.lothamer@gmail.com>
2025-06-08 21:09:41 -0300

    chore: delete deprecated frontend

--- web_frontend_deprecated/src/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import type { ReactElement } from "react";
-import type { AppProps } from "next/app";
-import "../styles/globals.css";
-
-export default function App({ Component, pageProps }: AppProps): ReactElement {
-    return <Component {...pageProps} />;
-}
